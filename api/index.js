92b31a993fce3f32004248399ce6a41e472d6ae5
nickoo20 <gniyati20@gmail.com>
2024-01-08 15:02:47 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from 'express' ;
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config() ;
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log('Connected To Mongoose ! ')
+})
+.catch((err)=>{
+    console.log(err) ; 
+});
+
 const app=express() ;
 const port=3000 ;
 
